0e43e6b85aa2e1270960db2652590b2c2ec39109
Quốc Khánh <bkdev98@gmail.com>
2018-06-28 17:53:28 +0700

    configs for heroku

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+  "client_id": "72c98205aad64d3da7772b8cd8e6984c",
+  "client_secret": "42941c74b2444ba39717516d2ba1149a",
+  "rescuetime_key": "B637DvrzKR0sW2S2fZaDm9DfjEMDX9L2vLRGeHLJ",
+  "refresh_token": "AQDGn45xTRtEuE4CyKOI45awFhQqbUPToFXDXkHw2HhK6DAhRkmT50th5EcFyaPP1bx80Xu6utE1T5U7ymYgtkUj_Beue3_r_AKrpYKdtVyFcMoHhfQ62CEmg7F3nrQhDtM"
+}
